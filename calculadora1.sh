e74e92211f5f45d189625ef2b2b2fbf58a7be9c1
Eduardo-ldc <ed224329@gmail.com>
2025-02-08 16:40:07 -0300

    Add files via upload

---
+++ calculadora1.sh
@@ -0,0 +1,37 @@
+Calculado
+
+def prompt_menu():
+    a = float(input("Escolha o primeiro número: "))
+    b = float(input("Escolha o segundo número: "))
+    print("""
+Choose an operation from the list:
+1. Adição
+2. Subtração
+3. Multiplicação
+4. Potência
+5. Divisão
+
+    """)
+    op = int(input("Enter the choice number: "))
+    return a, b, op
+
+def calculate():
+    a, b, op = prompt_menu()
+    if op == 1:
+        print("adição: {} + {} = {}".format(a,b,a+b))
+    elif op == 2:
+        print("subtração: {} - {} = {}".format(a,b,a-b))
+    elif op == 3:
+        print("multiplicação: {} * {} = {}".format(a,b,a*b))
+    elif op == 4:
+        print("potência: {}^{} = {}".format(a,b,a**b))
+    elif op == 5:
+        try:
+            print("divisão: {} / {} = {}".format(a,b,a/b))
+        except:
+            print("Divisão por 0 não é possível!")
+
+    else:
+        print("Não existe essa escolha!")
+    loop()
+
